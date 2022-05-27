fe54b842d7d996d6b1140f613c804a87574293ef
Vitaliy Alifanov <alifanov.vitalik@yandex.ru>
2022-05-27 15:46:55 +0300

    rasterizer rendering

--- src/renderer/rasterizer/rasterizer_renderer.cpp
+++ src/renderer/rasterizer/rasterizer_renderer.cpp
@@ -42,6 +42,17 @@ void cg::renderer::rasterization_renderer::render()
 {
 	rasterizer->clear_render_target({132, 234, 1});
 
+	float4x4 matrix = mul(
+			camera->get_projection_matrix(),
+			camera->get_view_matrix(),
+			model->get_world_matrix()
+	);
+
+	rasterizer->vertex_shader = [&](float4 vertex, cg::vertex vertex_data) {
+		auto processed = mul(matrix, vertex);
+		return std::make_pair(processed, vertex_data);
+	};
+
 	for (size_t shape_id = 0; shape_id < model->get_index_buffers().size(); ++shape_id) {
 		rasterizer->set_vertex_buffer(model->get_vertex_buffers()[shape_id]);
 		rasterizer->set_index_buffer(model->get_index_buffers()[shape_id]);
@@ -50,17 +61,6 @@ void cg::renderer::rasterization_renderer::render()
 				0
 				);
 	}
-
-	float4x4 matrix = mul(
-				camera->get_projection_matrix(),
-				camera->get_view_matrix(),
-				model->get_world_matrix()
-				);
-
-	rasterizer->vertex_shader = [&](float4 vertex, cg::vertex vertex_data) {
-		auto processed = mul(matrix, vertex);
-		return std::make_pair(processed, vertex_data);
-	};
 
 	// TODO: Lab 1.05. Implement `pixel_shader` lambda for the instance of `cg::renderer::rasterizer`
 
