d5357bd2879a4cb8c095da05cffecc03b1c8aa94
Vitaliy Alifanov <alifanov.vitalik@yandex.ru>
2022-06-23 19:13:00 +0300

    3.03 load assets

--- src/renderer/dx12/dx12_renderer.cpp
+++ src/renderer/dx12/dx12_renderer.cpp
@@ -258,7 +258,9 @@ void cg::renderer::dx12_renderer::load_assets()
 	index_buffers.resize(model->get_index_buffers().size());
 	index_buffer_views.resize(model->get_index_buffers().size());
 
-	for (size_t i = 0; i < model->get_index_buffers().size(); i++) {
+	for (size_t i = 0; i < model->get_index_buffers().size(); i++)
+	{
+		// Vertex buffer
 		auto vertex_buffer_data = model->get_vertex_buffers()[i];
 		const UINT vertex_buffer_size = static_cast<UINT>(
 				vertex_buffer_data->get_size_in_bytes()
@@ -270,6 +272,10 @@ void cg::renderer::dx12_renderer::load_assets()
 									   vertex_buffer_size,
 									   vertex_buffer_name
 									   );
+
+		copy_data(vertex_buffer_data->get_data(),
+				  vertex_buffer_size,
+				  vertex_buffers[i]);
 
 		auto index_buffer_data = model->get_index_buffers()[i];
 		const UINT index_buffer_size = static_cast<UINT>(
@@ -283,6 +289,10 @@ void cg::renderer::dx12_renderer::load_assets()
 									   index_buffer_name
 		);
 
+		copy_data(index_buffer_data->get_data(),
+				  index_buffer_size,
+				  index_buffers[i]);
+
 		// Constant buffer
 		std::wstring const_buffer_name(L"Constant buffer");
 		create_resource_on_upload_heap(
@@ -290,9 +300,18 @@ void cg::renderer::dx12_renderer::load_assets()
 				64 * 1024,
 				const_buffer_name
 				);
+
+		copy_data(&cb,
+				  sizeof(cb),
+				  constant_buffer);
+		CD3DX12_RANGE read_range(0, 0);
+		THROW_IF_FAILED(constant_buffer->Map(0,
+												  &read_range,
+												  reinterpret_cast<void**>(&constant_buffer_data_begin)
+														  )
+		);
 	}
 
-	// TODO Lab 3.03. Copy resource data to suitable resources
 	// TODO Lab 3.04. Create a descriptor heap for a constant buffer
 	// TODO Lab 3.04. Create a constant buffer view
 }
